266ff9193082072c3c02a9d185a93fa4d610657c
Paty Alves <patriciaalves10@gmail.com>
2020-06-20 18:34:51 -0300

    props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Header />
-      <Header />
-    </>
+    <div>
+      <Header title='Homepage'>
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title='Projects'>
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
+    </div>
   );
 }
 
