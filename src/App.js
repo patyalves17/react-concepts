402dadc02857e99575632a37a4d1168a91d7b531
Paty Alves <patriciaalves10@gmail.com>
2020-06-20 20:56:59 -0300

    adding projects on api

--- src/App.js
+++ src/App.js
@@ -14,8 +14,15 @@ function App() {
     });
   }, []);
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Paty',
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
   }
 
   return (
